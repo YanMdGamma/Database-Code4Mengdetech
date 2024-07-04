2f87c7140c95ff7811770f4c43ed6d85a2ed1b2c
YanMdGamma <114713847+YanMdGamma@users.noreply.github.com>
2024-07-04 11:07:51 +0800

    Delete Github_code/Python/Testing_procedure directory

--- Github_code/Python/Testing_procedure/Algorithm/dis.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-# 计算路径
-def dis_r(x_4, y_4, z_4, x_s4, y_s4, z_s4):
-    dis_r = np.sqrt((x_4 - x_s4)**2 + (y_4 - y_s4)**2 + (z_4 - z_s4)**2)
-    return dis_r
--- Github_code/Python/Testing_procedure/Data_storage/Print_Data2txt.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-def print_to_txt(filename):
-    sys.stdout = open(filename, 'w')
-
