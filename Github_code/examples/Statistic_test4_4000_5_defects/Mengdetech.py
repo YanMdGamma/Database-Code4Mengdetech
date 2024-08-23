62049b25a818519272cb385f66a248b91312d3cb
YanMdGamma <114713847+YanMdGamma@users.noreply.github.com>
2024-08-23 08:58:18 +0800

    Add files via upload

--- Github_code/examples/Statistic_test4_4000_5_defects/Mengdetech.py
+++ Github_code/examples/Statistic_test4_4000_5_defects/Mengdetech.py
@@ -1,17 +1,18 @@
 import main_detect
 import argparse
 from Algorithm import Change_xyz2use
+import time
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Enter your XYZ particle data file path.')
-    parser.add_argument('xyz_file_path', type=str, help='Path to the input XYZ file.')
+    parser.add_argument('--xyz_file_path', type=str, help='Path to the input XYZ file.', default='./use_file.xyz')
     parser.add_argument('--amplification_coefficient4abc', type=float, default=15.9427,
                         help='Amplification coefficient for point defects a,b,c (default: 15.9427)')
     parser.add_argument('--amplification_coefficient4de', type=float, default=13.8229,
                         help='Amplification coefficient for point defects d,e (default: 13.8229)')
     parser.add_argument('--atom_type', type=float, default=1,
                         help='Type of atom to be detected (default: 1)')
-    parser.add_argument('xyz_file_output_path', type=str, help='Path to the output XYZ file.')
+    parser.add_argument('--xyz_file_output_path', type=str, help='Path to the output XYZ file.', default='./output.xyz')
 
     args = parser.parse_args()
     # Show user command line reminder
@@ -22,11 +23,9 @@ if __name__ == "__main__":
     print(f"  XYZ output File : {args.xyz_file_output_path}")
     print(f"  Atom_type : {args.atom_type}")
 
-
-    # xyz_file_path = './use_file.xyz'
-
     mat_file_path = './n_0620_t1.mat'
     message_comment = Change_xyz2use.xyz_to_mat(args.xyz_file_path, mat_file_path)
+    print('message_comment: ', message_comment)
 
     main_detect.main(args.xyz_file_output_path, args.amplification_coefficient4de, args.amplification_coefficient4de,
                      args.atom_type, message_comment)
--- Github_code/examples/Statistic_test4_4000_5_defects/main_detect.py
+++ Github_code/examples/Statistic_test4_4000_5_defects/main_detect.py
@@ -5,11 +5,12 @@ from math import pi
 import matplotlib.pyplot as plt
 from scipy.interpolate import splrep, splev
 import my_module
-
+import time
 
 #
 # Load the data set and database type
 def main(xyz_file_output_path, amplification_coefficient4abc, amplification_coefficient4de, Atom_type, massage_comment):
+    start_time = time.time()
     data = sio.loadmat('n_0620_t1.mat')
     n_0620_t1 = data['test1']
     f_c_data = sio.loadmat('./database/f_c.mat')
@@ -59,8 +60,8 @@ def main(xyz_file_output_path, amplification_coefficient4abc, amplification_coef
     data = sio.loadmat('n_0620_t1.mat')
     Periodic_boundary_condition.replicative_solution(n_0620_t1, sphere_radium, x_max, x_min, y_max, y_min, z_max, z_min)
 
-    data_add = sio.loadmat('Replicative.mat')
-    n_0620_t1_add = data_add['mydata']
+    data_add = np.load('Replicative.npy')
+    n_0620_t1_add = data_add
     n_0620_t1_add = n_0620_t1_add[1:, :] # Delete a row with all zeros
     n_0620_t1_add = np.concatenate((n_0620_t1, n_0620_t1_add), axis=0)
 
@@ -166,9 +167,9 @@ def main(xyz_file_output_path, amplification_coefficient4abc, amplification_coef
     best_sim = np.zeros((n_0620_t1.shape[0], 4))
     similarity_score_0620_1 = np.zeros((percent_stand_test0620_1.shape[0], 6))
 
-    fig = plt.figure()
-    plt.plot(index, ie2[0, :])
-    plt.show()
+    # fig = plt.figure()
+    # plt.plot(index, ie2[0, :])
+    # plt.show()
 
     for i in range(n_0620_t1.shape[0]):
         test_line = test_0620_1[i, :]
@@ -222,8 +223,12 @@ def main(xyz_file_output_path, amplification_coefficient4abc, amplification_coef
 
     with open('output.txt', 'a') as file:
         # Call the function and pass the file object
+        end_time = time.time()
+        total_time = end_time - start_time
 
-        print('The ID of ia is: ', ia_detective, '\nThe ID of ib is: ', ib_detective, '\nThe ID of ic is: ',
+        print('------------------------------RESULTS------------------------------\n', file=file)
+        print('Total_time: ', total_time, '(s)/ ', total_time / 60, '(min)\n', file=file)
+        print('\nThe ID of ia is: ', ia_detective, '\nThe ID of ib is: ', ib_detective, '\nThe ID of ic is: ',
               ic_detective, file=file)
         print('The positions of ia is: ', ia_test0620_1[0:, 1:], '\nThe positions of ib is: ', ib_test0620_1[0:, 1:],
               '\nThe positions of ic is: ', ic_test0620_1[0:, 1:], file=file)
@@ -265,29 +270,4 @@ def main(xyz_file_output_path, amplification_coefficient4abc, amplification_coef
             x, y, z = map(float, ie_test52[i, 1:][0:])
             xyz_file.write(f"{ie_detective[i]} {Atom_type} {x} {y} {z} {4}\n")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    if id_test52.shape[0] != 0:
-        ax.scatter(id_test52[:, 1], id_test52[:, 2], id_test52[:, 3], c='yellow', marker='o', label='id_test52')
-
-    if ie_test52.shape[0] != 0:
-        ax.scatter(ie_test52[:, 1], ie_test52[:, 2], ie_test52[:, 3], c='cyan', marker='o', label='ie_test52')
-
-    if ia_test0620_1.shape[0] != 0:
-        ax.scatter(ia_test0620_1[:, 1], ia_test0620_1[:, 2], ia_test0620_1[:, 3], c='red', marker='o', label='ia_test0620_1')
-
-    if ib_test0620_1.shape[0] != 0:
-        ax.scatter(ib_test0620_1[:, 1], ib_test0620_1[:, 2], ib_test0620_1[:, 3], c='green', marker='o', label='ib_test0620_1')
-
-    if ic_test0620_1.shape[0] != 0:
-        ax.scatter(ic_test0620_1[:, 1], ic_test0620_1[:, 2], ic_test0620_1[:, 3], c='blue', marker='o', label='ic_test0620_1')
-
-    ax.set_xlabel(r'$x_{orientation}$/\AA')
-    ax.set_ylabel(r'$y_{orientation}$/\AA')
-    ax.set_zlabel(r'$z_{orientation}$/\AA')
-
-    plt.legend(loc='upper left')
-    plt.title('10230 particles_detect 10 defects')
-    plt.show()
 
